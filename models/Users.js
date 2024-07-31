ddc565c745eb5e958f78c493a40617ebccff4e0c
Harshit Mishra <harshit707125@gmail.com>
2024-07-31 22:16:50 +0530

    schema modified of users

--- models/Users.js
+++ models/Users.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  designation: { type: String,required: true, enum: ['Super admin', 'Temporary employee', 'Junior engineer'] },
+  designation: { type: String, required: true, enum: ['super admin', 'senior engineer', 'junior engineer', 'temporary employee'] },
 });
 
 const User = mongoose.model('User', UserSchema);
