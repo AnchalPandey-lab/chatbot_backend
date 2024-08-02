074c5791b70480d6cc58752387b7098c8ccc2303
Harshit Mishra <99409938+HarshitMishra55@users.noreply.github.com>
2024-08-02 16:05:20 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -7,22 +7,17 @@ const router = express.Router();
 
 // Register route
 router.post('/register', async (req, res) => {
-  const { username, email, password, designation } = req.body;
+  const { First_Name, Last_Name, Role, District, Institution_Name, Email, Password } = req.body;
   try {
-    let user = await User.findOne({ email });
+    let user = await User.findOne({ Email });
     if (user) {
       return res.status(400).json({ msg: 'User already exists' });
     }
 
-    user = new User({
-      username,
-      email,
-      password,
-      designation
-    });
+    user = new User({ First_Name, Last_Name, Role, District, Institution_Name, Email, Password });
 
     const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password, salt);
+    user.Password = await bcrypt.hash(Password, salt);
 
     await user.save();
     res.send('User registered successfully');
@@ -34,14 +29,14 @@ router.post('/register', async (req, res) => {
 
 // Login route
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+  const { Email, Password } = req.body;
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ Email });
     if (!user) {
       return res.status(400).json({ msg: 'Invalid credentials' });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(Password, user.Password);
     if (!isMatch) {
       return res.status(400).json({ msg: 'Invalid credentials' });
     }
@@ -49,7 +44,7 @@ router.post('/login', async (req, res) => {
     const payload = {
       user: {
         id: user.id,
-        designation: user.designation
+        role: user.Role
       }
     };
 
@@ -59,7 +54,7 @@ router.post('/login', async (req, res) => {
       { expiresIn: '1h' },
       (err, token) => {
         if (err) throw err;
-        res.json({ token, designation: user.designation });
+        res.json({ token, role: user.Role });
       }
     );
   } catch (err) {
